68ebadb761b8ef4cccdd4a0e6b039fd0897ba14f
Berenike Kuhli <berenike.kuhli@gmail.com>
2023-06-10 14:10:57 +0200

    Anlage Tables

--- libs/kk-easy-pay-common/src/lib/entities.ts
+++ libs/kk-easy-pay-common/src/lib/entities.ts
@@ -6,3 +6,20 @@ export interface LeistungsPaket {
    */
   price: number;
 }
+
+export interface InvoiceEntry {
+  id: number;
+  paket: LeistungsPaket;
+  amount: number;
+}
+
+export interface Invoice {
+  id: number;
+  firstName: string;
+  lastName: string;
+  insuranceNumber: string;
+  birthday: string;
+  month: number;
+  year: number;
+  entries: InvoiceEntry[];
+}
